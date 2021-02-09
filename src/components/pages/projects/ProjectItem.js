30dd85f84bfcf89a564be4203d0d26c186c8f0f7
AndrewMorrow <beardedmongo@gmail.com>
2021-02-09 12:33:39 -0600

    Adds icons to project card:

--- src/components/pages/projects/ProjectItem.js
+++ src/components/pages/projects/ProjectItem.js
@@ -21,8 +21,15 @@ import {
     FaExternalLinkSquareAlt,
     FaNodeJs,
     FaHtml5,
+    FaCss3Alt,
 } from "react-icons/fa";
 import { SiMysql, SiJavascript } from "react-icons/si";
+import { Icon, InlineIcon } from "@iconify/react";
+import handlebarsIcon from "@iconify-icons/logos/handlebars";
+import expressIcon from "@iconify-icons/logos/express";
+import sequelizeIcon from "@iconify-icons/logos/sequelize";
+import lodashIcon from "@iconify-icons/logos/lodash";
+import merrimentQuestSS from "../../../assets/images/merrimentQuestSS_tiny.png";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -54,11 +61,84 @@ const ProjectItem = ({ project }) => {
         setExpanded(!expanded);
     };
 
-    const checkIcons = (technologies) => {
+    const checkIcons = () => {
         technologies.map((tech) => {
             switch (tech) {
                 case "html":
-                    setTechIcons(...techIcons, "FaHtml5");
+                    return setTechIcons((state) => [
+                        ...state,
+                        <FaHtml5
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            size={30}
+                        />,
+                    ]);
+                case "css":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <FaCss3Alt
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            size={30}
+                        />,
+                    ]);
+                case "javascript":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <SiJavascript
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            size={30}
+                        />,
+                    ]);
+                case "node":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <FaNodeJs
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            size={30}
+                        />,
+                    ]);
+                case "express":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <Icon
+                            icon={expressIcon}
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                        />,
+                    ]);
+                case "mysql":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <SiMysql
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            size={30}
+                        />,
+                    ]);
+                case "sequelize":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <Icon
+                            icon={sequelizeIcon}
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            height="25px"
+                        />,
+                    ]);
+                case "handlebars":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <Icon
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            icon={handlebarsIcon}
+                            height="10px"
+                        />,
+                    ]);
+                case "lodash":
+                    return setTechIcons((state) => [
+                        ...state,
+                        <Icon
+                            style={{ marginLeft: "1rem", marginBottom: "1rem" }}
+                            icon={lodashIcon}
+                            height="25px"
+                        />,
+                    ]);
                 default:
                     return;
             }
@@ -66,9 +146,10 @@ const ProjectItem = ({ project }) => {
     };
 
     useEffect(() => {
-        // checkIcons();
+        checkIcons();
     }, []);
 
+    console.log(techIcons);
     return (
         <Card className={classes.root}>
             <CardHeader title={name} subheader="September 14, 2016" />
@@ -102,37 +183,8 @@ const ProjectItem = ({ project }) => {
             </CardActions>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add
-                        saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large,
-                        deep skillet over medium-high heat. Add chicken, shrimp
-                        and chorizo, and cook, stirring occasionally until
-                        lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo
-                        in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-                        onion, salt and pepper, and cook, stirring often until
-                        thickened and fragrant, about 10 minutes. Add saffron
-                        broth and remaining 4 1/2 cups chicken broth; bring to a
-                        boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with
-                        artichokes and peppers, and cook without stirring, until
-                        most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                        heat to medium-low, add reserved shrimp and mussels,
-                        tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just
-                        tender, 5 to 7 minutes more. (Discard any mussels that
-                        don’t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes,
-                        and then serve.
-                    </Typography>
+                    <Typography paragraph>Technologies:</Typography>
+                    {techIcons && techIcons.map((icon) => icon)}
                 </CardContent>
             </Collapse>
         </Card>
