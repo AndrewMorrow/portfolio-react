d6abd66633e06c7db3e3e042e9b141aa7e16f3e3
Andrew Morrow <beardedmongo@gmail.com>
2022-01-31 20:08:57 -0600

    Updates all resume references

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,28 +1,28 @@
 import React from "react";
 import ContactMailIcon from "@material-ui/icons/ContactMail";
 import { Fab, Tooltip } from "@material-ui/core";
-import resume from "../../../assets/files/Andrew_Morrow_Resume-2021.pdf";
+import resume from "../../../assets/files/Resume_2022.pdf";
 import DescriptionIcon from "@material-ui/icons/Description";
 
 const Footer = () => {
-    return (
-        <footer>
-            <a href="mailto:beardedmongo@gmail.com">
-                <Tooltip placement="left" title="Contact Me">
-                    <Fab size="small" className="contactFab">
-                        <ContactMailIcon fontSize="default" />
-                    </Fab>
-                </Tooltip>
-            </a>
-            <a href={resume} target="_blank">
-                <Tooltip placement="left" title="View Resume">
-                    <Fab size="small" className="resumeFab">
-                        <DescriptionIcon fontSize="default" />
-                    </Fab>
-                </Tooltip>
-            </a>
-        </footer>
-    );
+  return (
+    <footer>
+      <a href="mailto:beardedmongo@gmail.com">
+        <Tooltip placement="left" title="Contact Me">
+          <Fab size="small" className="contactFab">
+            <ContactMailIcon fontSize="default" />
+          </Fab>
+        </Tooltip>
+      </a>
+      <a href={resume} target="_blank" rel="noreferrer">
+        <Tooltip placement="left" title="View Resume">
+          <Fab size="small" className="resumeFab">
+            <DescriptionIcon fontSize="default" />
+          </Fab>
+        </Tooltip>
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/pages/contactPage/ContactPage.js
+++ src/components/pages/contactPage/ContactPage.js
@@ -1,90 +1,86 @@
 import React from "react";
 import {
-    Card,
-    CardContent,
-    Typography,
-    CardActions,
-    CardHeader,
-    Avatar,
-    Button,
-    Container,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  CardHeader,
+  Avatar,
+  Button,
+  Container,
 } from "@material-ui/core";
 import "./contact.css";
 import { FaLinkedin, FaGithub, FaAddressCard } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
 import { AiTwotoneMail } from "react-icons/ai";
 import avatarPic from "../../../assets/images/profileRoundFinal.png";
-import resume from "../../../assets/files/Andrew_Morrow_Resume-2021.pdf";
+import resume from "../../../assets/files/Resume_2022.pdf";
 import DescriptionIcon from "@material-ui/icons/Description";
 
 const ContactPage = () => {
-    return (
-        <main className="mainCont">
-            <Container maxWidth="xs">
-                <Card variant="outlined" className="contactCard">
-                    <CardContent>
-                        <Typography variant="h5" className="cardHeader">
-                            Contact Information
-                        </Typography>
-                        <CardHeader
-                            avatar={
-                                <Avatar alt="Andrew Morrow" src={avatarPic} />
-                            }
-                            className="cardHeader"
-                            title="Feel free to contact me!"
-                            subheader="Andrew Morrow"
-                        />
+  return (
+    <main className="mainCont">
+      <Container maxWidth="xs">
+        <Card variant="outlined" className="contactCard">
+          <CardContent>
+            <Typography variant="h5" className="cardHeader">
+              Contact Information
+            </Typography>
+            <CardHeader
+              avatar={<Avatar alt="Andrew Morrow" src={avatarPic} />}
+              className="cardHeader"
+              title="Feel free to contact me!"
+              subheader="Andrew Morrow"
+            />
 
-                        <Typography className="contactItems">
-                            <FaGithub style={{ marginRight: "4px" }} />
-                            Github:
-                            <a
-                                style={{ marginLeft: "4px" }}
-                                href="https://github.com/AndrewMorrow"
-                                target="_blank"
-                            >
-                                https://github.com/AndrewMorrow{" "}
-                                <FiExternalLink />
-                            </a>
-                        </Typography>
-                        <Typography className="contactItems">
-                            <FaLinkedin style={{ marginRight: "4px" }} />
-                            Linkedin:
-                            <a
-                                style={{ marginLeft: "4px" }}
-                                href="www.linkedin.com/in/andrewsmorrow"
-                                target="_blank"
-                            >
-                                www.linkedin.com/in/andrew-s-morrow{" "}
-                                <FiExternalLink />
-                            </a>
-                        </Typography>
-                        <Typography className="contactItems">
-                            <AiTwotoneMail style={{ marginRight: "4px" }} />
-                            Email:
-                            <a
-                                style={{ marginLeft: "4px" }}
-                                href="mailto:beardedmongo@gmail.com"
-                            >
-                                beardedmongo@gmail.com
-                            </a>
-                        </Typography>
-                        <div className="contactItems centered">
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                endIcon={<DescriptionIcon />}
-                                href={resume}
-                                target="_blank"
-                            >
-                                View Resume
-                            </Button>
-                        </div>
-                    </CardContent>
-                </Card>
-            </Container>
-        </main>
-    );
+            <Typography className="contactItems">
+              <FaGithub style={{ marginRight: "4px" }} />
+              Github:
+              <a
+                style={{ marginLeft: "4px" }}
+                href="https://github.com/AndrewMorrow"
+                target="_blank"
+              >
+                https://github.com/AndrewMorrow <FiExternalLink />
+              </a>
+            </Typography>
+            <Typography className="contactItems">
+              <FaLinkedin style={{ marginRight: "4px" }} />
+              Linkedin:
+              <a
+                style={{ marginLeft: "4px" }}
+                href="www.linkedin.com/in/andrewsmorrow"
+                target="_blank"
+              >
+                www.linkedin.com/in/andrew-s-morrow <FiExternalLink />
+              </a>
+            </Typography>
+            <Typography className="contactItems">
+              <AiTwotoneMail style={{ marginRight: "4px" }} />
+              Email:
+              <a
+                style={{ marginLeft: "4px" }}
+                href="mailto:beardedmongo@gmail.com"
+              >
+                beardedmongo@gmail.com
+              </a>
+            </Typography>
+            <div className="contactItems centered">
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={<DescriptionIcon />}
+                href={resume}
+                target="_blank"
+              >
+                View Resume
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </Container>
+    </main>
+  );
 };
 
 export default ContactPage;
